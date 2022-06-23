68b552acfab77a23fe669ddaeeb44d5a428b1306
hallyprince <86471995+hallyprince@users.noreply.github.com>
2022-06-23 11:30:41 +0100

    Add files via upload
    First JavaScript commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var fullname = "Sadiat Abu";
+var height = "6'5ft";
+var country = "Nigeria"
+
+
+
+document.write('My Name is ' + '' + fullname + '<br>');
+document.write('My Height is ' + '' + height + '<br>');
+document.write('I am from ' + '' + country + '<br>');
